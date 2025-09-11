004f524b94840a1d45f2887f4a1c919cd9a8b1df
bruno begnini <brunobegrib@gmail.com>
2025-09-11 20:56:53 -0300

    oi teste

--- src/Elementos/background.jsx
+++ src/Elementos/background.jsx
@@ -506,7 +506,7 @@ function Background() {
       <header className="absolute inset-x-0 top-0 z-50">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a className="-m-1.5 p-1.5">
               <span className="sr-only">KiwiLess</span>
               <img alt="KiwiLess" src="kiwilesslogo.png" width={90} className="mx-auto h-20 w-auto" />
             </a>
